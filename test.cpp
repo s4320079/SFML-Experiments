917ee1fcd57dae4318fd94440db98c1a8ea4a37d
Jacob Rogers <jacobgrantrogers@gmail.com>
2018-12-12 10:41:06 +1000

    experimented with drawing multiple shapes

--- test.cpp
+++ test.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(300, 300), "SFML works!");
+    sf::CircleShape circle = sf::CircleShape(100.f);
+    sf::CircleShape squares[4];
+    for(int ii = 0; ii < 4; ii++) {
+        squares[ii] = sf::CircleShape(75.f, 4);
+    }
+    squares[0].setPosition(75.f,-75.f);
+    squares[0].setFillColor(sf::Color(0xff, 0x00, 0x00));
+    squares[0].setRotation(45.f);
+    squares[1].setPosition(150.f,0.f);
+    squares[1].setRotation(45.f);
+    squares[1].setFillColor(sf::Color(0x00, 0xff, 0x00));
+    squares[2].setPosition(150.f,150.f);
+    squares[2].setRotation(45.f);
+    squares[2].setFillColor(sf::Color(0x00, 0x00, 0xff));
+    squares[3].setPosition(0.f,150.f);
+    squares[3].setFillColor(sf::Color(0xff, 0xff, 0xff));
+    squares[3].setRotation(45.f);
+    circle.setFillColor(sf::Color::Green);
     int i = 0;
     while (window.isOpen())
     {
@@ -16,12 +32,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        circle.setPosition(50.f, 50.f);
         window.clear();
-        window.draw(shape);
+
+        window.draw(circle);
+        for(int ii = 0; ii < 4; ii++) {
+            window.draw(squares[ii]);
+        }
         window.display();
         i = (i+255*255) | 255;
-        shape.setFillColor(sf::Color(i));
+        circle.setFillColor(sf::Color(i));
     }
     // cout << "Hello, World!";
     return 0;
